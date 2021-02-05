30faea029348d7c79b3b0efe14d1dcca2eab6057
Anna-Maria Eriksson <greatdanemaniac@gmail.com>
2021-02-05 15:54:01 +0100

    Put the play, search and create links in their right position with added animation

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="home">
 
-    <div class="play">
-      <h2>Play</h2>
+    <div class="MenuItem shake">
+      <router-link to="/play">Play</router-link>
     </div>
 
-    <div class="search">
-      <h2>Search</h2>
+    <div class="MenuItem shake">
+      <router-link to="/search">Search</router-link>
     </div>
 
-    <div class="create">
-      <h2>Create</h2>
+    <div class="MenuItem shake">
+      <router-link to="/create">Create</router-link>
     </div>
 
   </div>
@@ -32,6 +32,33 @@ export default {
   margin: auto;
 }
 
+.MenuItem {
+  margin-top: 20%;
+}
 
+.MenuItem a {
+  font-size: 70px;
+  color: black;
+  text-decoration: none;
+}
+
+.shake:hover {
+  animation: shake 0.5s;
+  animation-iteration-count: infinite;
+}
+
+@keyframes shake {
+  0% { transform: translate(1px, 1px) rotate(0deg); }
+  10% { transform: translate(-1px, -2px) rotate(-1deg); }
+  20% { transform: translate(-3px, 0px) rotate(1deg); }
+  30% { transform: translate(3px, 2px) rotate(0deg); }
+  40% { transform: translate(1px, -1px) rotate(1deg); }
+  50% { transform: translate(-1px, 2px) rotate(-1deg); }
+  60% { transform: translate(-3px, 1px) rotate(0deg); }
+  70% { transform: translate(3px, 1px) rotate(-1deg); }
+  80% { transform: translate(-1px, -1px) rotate(1deg); }
+  90% { transform: translate(1px, 2px) rotate(0deg); }
+  100% { transform: translate(1px, -2px) rotate(-1deg); }
+}
 
 </style>
