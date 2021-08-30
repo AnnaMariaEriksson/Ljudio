5f651c21065a23d2a5d21b5648a40f224a766cb1
Anna-Maria Eriksson <greatdanemaniac@gmail.com>
2021-08-30 14:32:57 +0200

    Added some info about the DB connection not working

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h2>Please observe - the DB connection does not work for some strange reason. I'm trying to fix that.</h2>
+    <h3>Complete code can be found at <a href="https://github.com/AnnaMariaEriksson/ljudio">Github</a> </h3>
   </div>
 </template>
