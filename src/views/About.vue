dd4a00d6214f7b8977523fcb409850fc18de986f
Anna-Maria Eriksson <greatdanemaniac@gmail.com>
2021-11-03 14:40:46 +0100

    Updated About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,13 +2,8 @@
   <div class="about">
     <h1>This is an about page</h1>
 
-    <h3 v-if="message=!null">If you can see this, we've successfully fetched a message from netlify's functions folder. <br>
-    {{message}}
-    </h3>
-
-
-    <h2>Please observe - the DB connection does not work for some strange reason. I'm trying to fix that.</h2>
-    <h3>Complete code can be found at <a href="https://github.com/AnnaMariaEriksson/ljudio">Github</a> </h3>
+    <h2>The DB connection finally works in production mode, but there might still be some hiccups!</h2>
+    <h3>Complete code can be found at <a href="https://github.com/AnnaMariaEriksson/ljudio">Github.</a> </h3>
   </div>
 </template>
 
