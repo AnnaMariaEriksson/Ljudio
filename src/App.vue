d7084a94b4260dd9460d60021e923747224b86e9
Anna-Maria Eriksson <greatdanemaniac@gmail.com>
2021-11-03 15:11:31 +0100

    Added information about all ze bugs

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,10 @@ export default {
 
   mounted() {
 this.$store.dispatch("checkAuth")
+    alert('Hello, there... Just so you know, it will take a while until something loads here. \n \n'  +
+      'That is because the backend is hosted at Heroku and I cannot get cron jobs atm. \n \n' +
+      'So huge thanks for bearing with me here...! \n \n' +
+    'Also there might be an issue with signin in etc, but I am working all these little bugs out in time.')
   },
   computed: {
     user() {
